9c48d26220fea008bff02e506fa47f39365ded07
lidashuai12 <92199991+lidashuai12@users.noreply.github.com>
2022-09-30 12:44:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -106,8 +106,7 @@ def get_ciba():
     url = "http://open.iciba.com/dsapi/"
     headers = {
         'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53'
     }
     r = get(url, headers=headers)
     note_en = r.json()["content"]
